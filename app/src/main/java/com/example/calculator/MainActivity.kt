f5fdc56b12b85eff5db3b1559e41240ef948f691
sumedhdravid <sumedhdravid98@gmail.com>
2020-08-31 16:40:54 +0530

    Broadcast Receivers 1

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.onlineandroid.R
 
 class MainActivity : AppCompatActivity() {
 
